2def433d3cb824f5562167ae463382919610df89
Zainal Abidin <zabidin384@gmail.com>
2026-01-02 10:08:02 +0700

    Fix allowed origins

--- server.js
+++ server.js
@@ -17,19 +17,18 @@ import userRouter from "./routes/userRoute.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
 await connectDB();
 await connectCloudinary();
-
-// Allow multiple origins
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
 // Middleware configuration
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }));
 
+// Routes
 app.get("/", (req, res) => res.send("API is working!"));
 app.use("/api/user", userRouter);
 app.use("/api/seller", sellerRouter);
